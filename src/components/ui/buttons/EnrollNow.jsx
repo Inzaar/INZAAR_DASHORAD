d35d0b95782a960e7f3e3d4a34c0ac3a4bb86919
muzamilhussain33 <muzamilhussain369@gmail.com>
2025-11-23 00:19:29 +0500

    set folder structure

---
+++ src/components/ui/buttons/EnrollNow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EnrollNow() {
+  return (
+    <div className='w-[81px] h-[26px] bg-linear-'>
+      Enroll Button
+    </div>
+  )
+}
+
+export default EnrollNow
