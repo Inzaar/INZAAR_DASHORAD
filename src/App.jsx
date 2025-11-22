4964ab77f1eb1383ce9094f1f7b63c763200095d
muzamilhussain33 <muzamilhussain369@gmail.com>
2025-11-23 02:13:24 +0500

    create course card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import GradiantButton from "./components/ui/buttons/GradiantButton"
+import { CourseCard } from "./features/courses"
+import thumbnail from "./assets/images/course2.png"
+import profile from "./assets/icons/profile.png"
 
 function App() {
 
+  const course = {
+    title: "Introduction to React",
+    time: "10 Lectures (3 hours, 23 minutes, 24 seconds)",
+    category: "Web Development",
+    description: "Course explains Day of Judgment using Quranic verses and authentic Ahadith.",
+    price: 49.99,
+    thumbnail: thumbnail,
+    icon: profile
+  }
+
+
   return (
     <div>
-      
+      <GradiantButton className="p-4 rounded-lg h-[100px]">Enroll Now</GradiantButton>
+      <CourseCard course={course}/>
     </div>
   )
 }
--- src/components/ui/buttons/GradiantButton.jsx
+++ src/components/ui/buttons/GradiantButton.jsx
@@ -1,9 +1,9 @@
 
-function GradiantButton({className, content}) {
+function GradiantButton({className, children}) {
   return (
     <div className={` text-white bg-gradient-to-r from-[#3758EE] to-[#B666E7] flex items-center justify-center ${className}`}>
       {/* Enroll Now */}
-      {content}
+      {children}
     </div>
   )
 }
