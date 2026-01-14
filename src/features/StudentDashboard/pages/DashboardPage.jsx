dbb22131bd3e02108cd75b3cdc9fb1c68039d5b0
muzamilhussain33 <muzamilhussain369@gmail.com>
2026-01-14 17:52:12 +0500

    fix some problems in the dashborad

--- src/features/StudentDashboard/pages/DashboardPage.jsx
+++ src/features/StudentDashboard/pages/DashboardPage.jsx
@@ -9,6 +9,7 @@ import StatusTable from '@/components/ui/statusTable/StatusTable';
 import Analytics from '../components/Analytics';
 import EnrolledCourse from '../components/EnrolledCourse';
 import { useNavigate } from 'react-router-dom';
+import { Calendar } from 'lucide-react';
 
 const DashboardPage = () => {
     const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
