bab7859f057d1bd3ac37ec73fdaa4798aa7dcbeb
muzamilhussain33 <muzamilhussain369@gmail.com>
2026-02-03 21:01:47 +0500

    remove extra things

--- src/features/courses/pages/Courses.jsx
+++ src/features/courses/pages/Courses.jsx
@@ -32,7 +32,7 @@ const Courses = () => {
                     setCourses(res.data.data);
                 }
             } catch (error) {
-                console.error("Failed to fetch courses:", error);//git fixed
+                console.error("Failed to fetch courses:", error);
             } finally {
                 setLoading(false);
             }
