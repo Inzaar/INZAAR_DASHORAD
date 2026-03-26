02ea537b8a559b8a620cb3cb702e3763623f2de5
Muzamil Hussain <muzamilhussain369@gmail.com>
2026-03-26 15:51:57 +0500

    add moderator detail page

--- src/features/adminDashborad/components/moderator/ModeratorProfile.jsx
+++ src/features/adminDashborad/components/moderator/ModeratorProfile.jsx
@@ -3,13 +3,14 @@ import { FaIdCard } from "react-icons/fa";
 
 function ModeratorProfile() {
   return (
-    <div className="w-[1120px] h-[687px] rounded-[10px] border border-[#ECECEC] p-[14px]">
-      <div className="w-[1092px] h-[639px] mx-auto">
+    <div className="w-full rounded-[10px] border border-[#ECECEC] p-[14px]">
+      <div className="w-full mx-auto">
+
         {/* Header */}
-        <div className="w-full h-[40px] flex justify-between items-center">
+        <div className="w-full flex justify-between items-center">
           <h3 className="text-[18px] font-medium">Profile Details</h3>
 
-          <div className="w-[279px] h-[40px] flex gap-[12px]">
+          <div className="flex gap-[12px]">
             <button className="w-[177px] h-[40px] bg-[#A7A7A7] rounded-[4px] text-white text-sm">
               Change Password
             </button>
@@ -19,132 +20,131 @@ function ModeratorProfile() {
           </div>
         </div>
 
-        <div className="w-full h-[547px] flex mt-[20px] gap-[20px]">
-          {/* Left Side */}
-          <div className="w-[546px] h-full flex flex-col gap-[18px]">
-            {/* First Name */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">First name</label>
-              <input
-                type="text"
-                placeholder="Enter first name"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
-              />
-            </div>
+        {/* Flex Form */}
+        <div className="w-full mt-[20px] flex flex-wrap gap-[20px]">
 
-            {/* Number */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">Number</label>
-              <div className="flex items-center h-[48px] px-3 gap-2 rounded-md border border-[#E4E4E7]">
-                <select className="outline-none bg-transparent">
-                  <option>🇺🇸 +1</option>
-                  <option>🇵🇰 +92</option>
-                  <option>🇮🇳 +91</option>
-                </select>
-                <input
-                  type="tel"
-                  placeholder="Phone number"
-                  className="outline-none w-full"
-                />
-              </div>
-            </div>
-
-            {/* CNIC */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">CNIC</label>
-              <input
-                type="text"
-                placeholder="23456-2389-1"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
-              />
-            </div>
-
-            {/* Nationality */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">Nationality</label>
-              <input
-                type="text"
-                placeholder="Enter Your Nationality"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
-              />
-            </div>
-
-            {/* Front CNIC */}
-            <div>
-              <p className="mb-2 text-[14px] font-medium">Front CNIC</p>
-              <div className="w-[536px] h-[125px] rounded-[10px] border border-[#E4E4E7] flex justify-center items-center">
-                <div className="flex flex-col items-center gap-2">
-                  <FaIdCard className="w-[46px] h-[30px] text-[#A7A7A7]" />
-                  <p className="text-[10px] text-[#666666]">
-                    Upload Front CNIC
-                  </p>
-                  <label className="bg-[#265CEB] rounded-[4px] text-white w-[60px] h-[24px] text-[10px] flex items-center justify-center cursor-pointer">
-                    Browse
-                    <input type="file" className="hidden" />
-                  </label>
-                </div>
-              </div>
-            </div>
+          {/* First Name */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-1 lg:order-1">
+            <label className="font-medium text-[14px]">First name</label>
+            <input
+              type="text"
+              placeholder="Enter first name"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
           </div>
 
-          {/* Right Side */}
-          <div className="w-[546px] h-full flex flex-col gap-[18px]">
-            {/* Email */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">Email</label>
-              <input
-                type="text"
-                placeholder="Enter email"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
-              />
-            </div>
+          {/* Email */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-5 lg:order-2">
+            <label className="font-medium text-[14px]">Email</label>
+            <input
+              type="text"
+              placeholder="Enter email"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
+          </div>
 
-            {/* Gender */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">Gender</label>
-              <select className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none">
-                <option>Choose</option>
-                <option>Male</option>
-                <option>Female</option>
+          {/* Number */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-2 lg:order-3">
+            <label className="font-medium text-[14px]">Number</label>
+            <div className="flex items-center h-[48px] px-3 gap-2 rounded-md border border-[#E4E4E7]">
+              <select className="outline-none bg-transparent">
+                <option>🇺🇸 +1</option>
+                <option>🇵🇰 +92</option>
+                <option>🇮🇳 +91</option>
               </select>
-            </div>
-
-            {/* Address */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">Address</label>
               <input
-                type="text"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+                type="tel"
+                placeholder="Phone number"
+                className="outline-none w-full"
               />
             </div>
+          </div>
 
-            {/* Higher Education */}
-            <div className="flex flex-col gap-[8px]">
-              <label className="font-medium text-[14px]">
-                Higher Education
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Your Education"
-                className="w-[536px] h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
-              />
-            </div>
+          {/* Gender */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-6 lg:order-4">
+            <label className="font-medium text-[14px]">Gender</label>
+            <select className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none">
+              <option>Choose</option>
+              <option>Male</option>
+              <option>Female</option>
+            </select>
+          </div>
 
-            {/* Back CNIC */}
-            <div>
-              <p className="mb-2 text-[14px] font-medium">Back CNIC</p>
-              <div className="w-[536px] h-[125px] rounded-[10px] border border-[#E4E4E7] flex justify-center items-center">
-                <div className="flex flex-col items-center gap-2">
-                  <FaIdCard className="w-[46px] h-[30px] text-[#A7A7A7]" />
-                  <p className="text-[10px] text-[#666666]">Upload Back CNIC</p>
-                  <label className="bg-[#265CEB] rounded-[4px] text-white w-[60px] h-[24px] text-[10px] flex items-center justify-center cursor-pointer">
-                    Browse
-                    <input type="file" className="hidden" />
-                  </label>
-                </div>
+          {/* CNIC */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-3 lg:order-5">
+            <label className="font-medium text-[14px]">CNIC</label>
+            <input
+              type="text"
+              placeholder="23456-2389-1"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
+          </div>
+
+          {/* Address */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-7 lg:order-6">
+            <label className="font-medium text-[14px]">Address</label>
+            <input
+              type="text"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
+          </div>
+
+          {/* Nationality */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-4 lg:order-7">
+            <label className="font-medium text-[14px]">Nationality</label>
+            <input
+              type="text"
+              placeholder="Enter Your Nationality"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
+          </div>
+
+          {/* Higher Education */}
+          <div className="flex flex-col gap-[8px] w-full lg:w-[48%] order-8 lg:order-8">
+            <label className="font-medium text-[14px]">
+              Higher Education
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Education"
+              className="w-full h-[48px] rounded-md px-3 border border-[#E4E4E7] outline-none"
+            />
+          </div>
+
+          {/* Front CNIC */}
+          <div className="w-full lg:w-[48%] order-9 lg:order-9">
+            <p className="mb-2 text-[14px] font-medium">Front CNIC</p>
+            <div className="w-full h-[125px] rounded-[10px] border border-[#E4E4E7] flex justify-center items-center">
+              <div className="flex flex-col items-center gap-2">
+                <FaIdCard className="w-[46px] h-[30px] text-[#A7A7A7]" />
+                <p className="text-[10px] text-[#666666]">
+                  Upload Front CNIC
+                </p>
+                <label className="bg-[#265CEB] rounded-[4px] text-white w-[60px] h-[24px] text-[10px] flex items-center justify-center cursor-pointer">
+                  Browse
+                  <input type="file" className="hidden" />
+                </label>
               </div>
             </div>
           </div>
+
+          {/* Back CNIC */}
+          <div className="w-full lg:w-[48%] order-10 lg:order-10">
+            <p className="mb-2 text-[14px] font-medium">Back CNIC</p>
+            <div className="w-full h-[125px] rounded-[10px] border border-[#E4E4E7] flex justify-center items-center">
+              <div className="flex flex-col items-center gap-2">
+                <FaIdCard className="w-[46px] h-[30px] text-[#A7A7A7]" />
+                <p className="text-[10px] text-[#666666]">
+                  Upload Back CNIC
+                </p>
+                <label className="bg-[#265CEB] rounded-[4px] text-white w-[60px] h-[24px] text-[10px] flex items-center justify-center cursor-pointer">
+                  Browse
+                  <input type="file" className="hidden" />
+                </label>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
--- src/features/adminDashborad/components/moderator/ModeratorProfileComponent.jsx
+++ src/features/adminDashborad/components/moderator/ModeratorProfileComponent.jsx
@@ -3,19 +3,37 @@ import ModeratorRoll from "./ModeratorRoll";
 import ModeratorProfile from "./ModeratorProfile";
 
 
+// function ModeratorProfileComponent () {
+//     return (
+//         <div>
+//             <div className="mt-[20px] w-full">
+//                               <div className="flex w-[1120px] h-[301px] gap-[10px]">
+//                                 <ModeratorRoll/>
+//                                 <SessionActivity />
+//                               </div>
+//                             </div>
+//                             <div className="mt-[12px] w-full">
+//                               <ModeratorProfile/>
+//                             </div>
+//         </div>
+//     )
+// }
+// export default ModeratorProfileComponent;
 function ModeratorProfileComponent () {
     return (
-        <div>
+        <div className="w-full">
             <div className="mt-[20px] w-full">
-                              <div className="flex w-[1120px] h-[301px] gap-[10px]">
-                                <ModeratorRoll/>
-                                <SessionActivity />
-                              </div>
-                            </div>
-                            <div className="mt-[12px] w-full">
-                              <ModeratorProfile/>
-                            </div>
+                <div className="flex flex-col lg:flex-row w-full gap-[10px]">
+                    <ModeratorRoll/>
+                    <SessionActivity />
+                </div>
+            </div>
+
+            <div className="mt-[12px] w-full">
+                <ModeratorProfile/>
+            </div>
         </div>
     )
 }
+
 export default ModeratorProfileComponent;
